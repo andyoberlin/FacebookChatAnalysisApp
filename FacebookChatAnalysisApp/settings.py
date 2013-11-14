22e9d39e1b2db37e38cb327e75932f6f3afb6cab
Andrew Oberlin <andyoberlin@gmail.com>
2013-11-14 03:16:14 -0500

    Changed the settings to correctly setup the static and media directories.

--- FacebookChatAnalysisApp/settings.py
+++ FacebookChatAnalysisApp/settings.py
@@ -86,4 +86,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'FacebookChatAnalysisApp/media')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = SITE_URL + 'media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'FacebookChatAnalysisApp/static')
+
 STATIC_URL = SITE_URL + 'static/'
