859819dd9074741d114d6ac74af7c8d01190bb0e
Andrew Oberlin <andyoberlin@gmail.com>
2013-11-20 22:41:22 -0500

    Added first attempt at storage of the messages

--- main/static/js/database/MessagesSDK.js
+++ main/static/js/database/MessagesSDK.js
@@ -65,28 +65,39 @@ define(['jquery', 'facebook', 'persistence_store_web_sql'], function($, FB, pers
 			});
 		}
 		else {
-			FB.api('/' + this.conversation + '/comments', function(response) {
-				if (response && response.data) {
-					// get the message count by analyzing the id of the most recent message
-					var numMessages = parseInt(response.data[response.data.length - 1].id.split("_")[1]);
-					
-					if (!isNaN(numMessages)) {
-						self.state.totalMessages = numMessages;
+			FB.api('/fql?q=SELECT+message+FROM+thread+WHERE+thread_id+=+' + self.conversation, function(response) {
+				if (response && response.data && response.data.lemgth > 0) {
+					self.state.totalMessages = response.data[0].message_count;
+				}
+				
+				FB.api('/' + this.conversation + '/comments', function(response) {
+					if (response && response.data) {
+						// get the message count by analyzing the id of the most recent message
+						if (!self.state.totalMessages) {
+							var numMessages = parseInt(response.data[response.data.length - 1].id.split("_")[1]);
+							
+							if (!isNaN(numMessages)) {
+								self.state.totalMessages = numMessages;
+							}
+						}
+						
 						self.state.completeMessages = response.data.length;
+						
+						$(self).trigger('sdk.update');
+						
+						self.storeMessages(response.data);
+						self.fetchOldMessages(response.paging.next);
 					}
-					
-					$(self).trigger('sdk.update');
-					
-					self.storeMessages(response.data);
-					self.fetchOldMessages(response.paging.next);
-				}
-				else {
-					self.state.updating = false;
-					self.state.message = "Completed downloading messages.";
-					$(self).trigger('sdk.complete');
-				}
+					else {
+						self.state.updating = false;
+						self.state.message = "Completed downloading messages.";
+						$(self).trigger('sdk.complete');
+					}
+				});
 			});
 		}
+		
+		
 	};
 	
 	MessagesSDK.prototype.fetchNewMessages = function(lastMessage) {
@@ -110,8 +121,23 @@ define(['jquery', 'facebook', 'persistence_store_web_sql'], function($, FB, pers
 	
 	MessagesSDK.prototype.initializeDatabase = function() {
 		if (!this.initialized) {
-			//persistence.store.websql.config(persistence, 'conversation_' + this.conversation,
-			//	'Stores the messages from Facebook for analysis purposes', 10 * 1024 * 1024);
+			persistence.store.websql.config(persistence, 'conversation_' + this.conversation,
+				'Stores the messages from Facebook for analysis purposes', 10 * 1024 * 1024);
+			
+			this.MessageModel = persistence.define('Message', {
+				uid: "TEXT",
+				message: "TEXT",
+				time: "DATE"
+			});
+			
+			this.FriendModel = persistence.define('Friend', {
+				uid: "TEXT",
+				name: "TEXT"
+			});
+			
+			this.MessageModel.hasOne('from', this.FriendModel);
+			
+			persistence.syncSchema();
 			
 			this.initialized = true;
 		}
@@ -119,7 +145,33 @@ define(['jquery', 'facebook', 'persistence_store_web_sql'], function($, FB, pers
 	
 	MessagesSDK.prototype.storeMessages = function(messages) {
 		if (this.initialized) {
-			console.log("Stored messages");
+			console.log("Storing messages...");
+			
+			var self = this;
+			
+			$.each(messages, function(index, message) {
+				self.FriendModel.findBy('uid', message.from.id, function(friend) {
+					if (!friend) {
+						friend = new self.FriendModel({
+							uid: message.from.id,
+							name: message.from.name
+						});
+						
+						persistence.add(friend);
+					}
+					
+					var msg = new self.MessageModel({
+						uid: message.id,
+						message: message.message ? message.message : "NULL",
+						from: friend,
+						time: Date.parse(message.created_time)
+					});
+					
+					persistence.add(msg);
+				});
+			});
+			
+			console.log("Finished storing messages.");
 		}
 		else {
 			console.log("Trying to store messages before database initialization.");
