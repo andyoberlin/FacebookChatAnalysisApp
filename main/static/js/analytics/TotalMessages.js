3e4d5603b5706e1669b29b648aa6deac004cb736
Michael Sinko <sinkomr@miamioh.edu>
2013-11-21 00:19:47 -0500

    Added total stickers analytic

---
+++ main/static/js/analytics/TotalMessages.js
@@ -0,0 +1,28 @@
+define(['jquery', 'database/DatabaseUtil'], function($, DatabaseUtil) {
+	var Analytic = {
+		run: function(conversation, callback) {
+			var dbUtil = DatabaseUtil.createInstance(conversation);
+		
+			$.when(dbUtil.getUsers()).then(function(users) {
+				var promises = [];
+				var list = {};
+		
+				$.each(users, function(index, user) {
+					promises.push(
+						dbUtil.getMessages('person', user).then(
+							function(messages) {
+								list[user.name] = messages.length;
+							}
+						)
+					); 
+				});
+				
+				$.when.apply($, promises).then(function() {
+					callback(list);
+				});
+			});	
+		}
+	};
+	
+	return Analytic;
+});
